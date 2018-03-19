f5296de96e083c794e4e32a6802993adbb419262
winnerwly <975080391@qq.com>
2018-03-19 09:55:30 +0800

    ..

---
+++ src/utils/index.js
@@ -0,0 +1,26 @@
+import { getToken } from './authority';
+import { request as query } from './query';
+import { request } from './request';
+
+function urlToObj(url) {
+  const urlStr = url.split('?')[1];
+  if (urlStr) {
+    const urlArr = urlStr.split('&');
+    const urlObj = {};
+    urlArr.forEach((item) => {
+      const [key, val] = item.split('=');
+      urlObj[key] = val;
+    });
+    console.log('urlObj', urlObj);
+    return urlObj;
+  } else {
+    return {};
+  }
+}
+
+export {
+  getToken,
+  query,
+  request,
+  urlToObj,
+};
