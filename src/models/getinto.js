ea3623eb05588b475e23f0407261521f1b7c3669
winnerwly <975080391@qq.com>
2018-03-30 15:08:31 +0800

    ...

--- src/models/getinto.js
+++ src/models/getinto.js
@@ -2,6 +2,7 @@ import modelExtend from 'dva-model-extend';
 // import { notification } from 'antd';
 import { pageModel } from './common';
 import { query } from '../services/query/getinto';
+import { query as feeQuery } from '../services/setting/warehouse-fee';
 
 export default modelExtend(pageModel, {
   namespace: 'getinto',
@@ -25,6 +26,8 @@ export default modelExtend(pageModel, {
       currentPage: 1,
       pageSize: 10,
     } }, { call, put }) {
+      const feeRes = yield call(feeQuery);
+      console.log('feeRes', feeRes);
       const data = yield call(query, {
         ...payload,
         currentPage: Number(payload.currentPage) || 1,
@@ -32,7 +35,19 @@ export default modelExtend(pageModel, {
       });
       if (data.code === 200) {
         const list = data.data.map((item) => {
-          return { key: item.id, ...item };
+          let cargoCharge = '未获取';
+          if (feeRes.code === 200) {
+            const tmpTime = new Date().getTime() - item.scan_time;
+            if (tmpTime > feeRes.data[0].expire_time) {
+              const tmpCount = Math.ceil(tmpTime / 86400000);
+              cargoCharge = `${tmpCount * feeRes.data[0].cargo_fee} 元`;
+            }
+          }
+          return {
+            key: item.id,
+            ...item,
+            cargo_charge: cargoCharge,
+          };
         });
         yield put({
           type: 'setStates',
